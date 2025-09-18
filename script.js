783110ce8a3c1dce96de1c7c504320311e3244a9
Muneeb Siddiqui <muneebsiddiqui95@gmail.com>
2025-09-18 17:15:21 +0500

    update notes

---
+++ script.js
@@ -0,0 +1,10 @@
+/* var a = +prompt("Give any number")
+for (var index = 1; index < 21; index++) {
+    console.log(index * 5)
+} */
+
+var num = +prompt("Give any number")
+var a = 6;
+var b = 8;
+var c = 12;
+alert(a + b + c + num)
